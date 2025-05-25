08952abcca4b05c763e0fff8f21730bae741f80b
Charles Stoeter <charlesstoeter@gmail.com>
2025-05-25 10:35:57 -0400

    worked on logic.h file

--- Game Program Assignment 1/logic.cpp
+++ Game Program Assignment 1/logic.cpp
@@ -1,1 +1,10 @@
 #include "logic.h"
+#include <algorithm>
+#include <ctime>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+
--- Game Program Assignment 1/logic.h
+++ Game Program Assignment 1/logic.h
@@ -1,11 +1,7 @@
 #pragma once
 #include <vector>
 #include <string>
-#include <iostream>
-#include <fstream>
-#include <algorithm>
-#include <ctime>
-#include <cstdlib>
+
 
 
 
@@ -15,33 +11,23 @@ class logic
 public:
 	logic(); //constructor
 
-	void loadDictionary(); //load dictionary from file
-	void scrambleWord(const std::string& word); //scramble a word
-	void checkAnswer(const std::string& answer); //check player's answer
-	void nextRound(); //proceed to the next round
+	void loadWordsFromFile(const std::string filename);
+	void selectRandomWords();
+	void scrambleWords();
+	void play();
+	void showResults();
 
 
 private:
 	std::vector<std::string> list1; //4-5 letter words
 	std::vector<std::string> list2; //6-7 letter words
 	std::vector<std::string> list3; //8+ letter words
+	std::vector<std::string> selectedWords;
 	std::vector<std::string> scrambledWords; 
 	std::vector<std::string> correctWords; 
-	int score = 0; 
-	int round = 1; 
+
 
 };
 
-logic::logic() {
-	loadDictionary();
-}
 
-void logic::loadDictionary() {
-	std::ifstream file("dictionary.txt");
-	if (!file) {
-		std::cerr << "Error opening dictionary file.\n";
-		return -1;
-	}
-	
-}
 
