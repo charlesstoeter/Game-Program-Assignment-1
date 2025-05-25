944495890624fd2a5a8abd338d3bdbc5bdd264ba
Charles Stoeter <charlesstoeter@gmail.com>
2025-05-25 19:24:20 -0400

    added time mechanic in main

--- Game Program Assignment 1/Game Program Assignment 1.cpp
+++ Game Program Assignment 1/Game Program Assignment 1.cpp
@@ -20,13 +20,47 @@ int main() {
 	}
 	
 
-	ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-	if (!queue) {
+	ALLEGRO_EVENT_QUEUE* event_queue = al_create_event_queue();
+	if (!event_queue) {
 		std::cerr << "Failed to create event queue." << std::endl;
 		al_destroy_timer(timer);
 		return -1;
 	}
 
+
+	al_register_event_source(event_queue, al_get_timer_event_source(timer));
+
+	logic game;
+	game.loadWordsFromFile("dictionary.txt");
+	game.selectRandomWords();
+	game.scrambleWords();
+
+	//starts the timer
+	al_start_timer(timer);
+
+	int secondsPassed = 0;
+
+	while (true) {
+		ALLEGRO_EVENT event;
+		al_wait_for_event(event_queue, &event);
+
+		//if type statements
+		if (event.type == ALLEGRO_EVENT_TIMER) {
+			secondsPassed++;
+
+
+			game.play(secondsPassed);
+			break;
+		}
+	}
+
+
+	al_destroy_timer(timer);
+	al_destroy_event_queue(event_queue);
+
+	game.showResults();
+
+	return 0;
 }
 
 
